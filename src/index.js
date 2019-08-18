f69f7f9bb485f5688cd7205622b7a536c0832df1
Mike <michael.barbone@gmail.com>
2019-08-18 13:31:55 -0400

    Write as a class component and successfully grabbing user location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>hey</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));

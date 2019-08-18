57143bcdc2497661f09e493d81c0b81c1383e106
Mike <michael.barbone@gmail.com>
2019-08-18 18:16:19 -0400

    Determine season in user location and render text according to the season

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMSG: "" };
 
-    this.state = { lat: null };
-
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
         console.log(position);
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        console.log(err);
+        this.setState({ errorMSG: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMSG && !this.state.lat) {
+      return <div>Error: {this.state.errorMSG}</div>;
+    }
+
+    if (!this.state.errMSG && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
